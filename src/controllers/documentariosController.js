17cdd5ecc87be800f8e3f21d02d5ee40979a3281
Luanafollegati <luana.guerreiro@aluno.senai.br>
2025-09-22 09:30:52 -0300

    feat: finaliza create

--- src/controllers/documentariosController.js
+++ src/controllers/documentariosController.js
@@ -3,65 +3,110 @@ const { documentarios } = dados;
 
 //Listar todos
 const getAllDocumentarios = (req, res) => {
-    res.status(200).json({
-        total: documentarios.length,
-        documentarios: documentarios
-    })
+  res.status(200).json({
+    total: documentarios.length,
+    documentarios: documentarios,
+  });
 };
 
 //Pegar por id
 const getDocumentariosById = (req, res) => {
-    let id = parseInt(req.params.id);
+  let id = parseInt(req.params.id);
 
-    const documentario = documentarios.find(d => d.id === id);
+  const documentario = documentarios.find((d) => d.id === id);
 
-    if (documentario) {
-        res.status(200).json({
-            success: true,
-            documentario: documentario
-        })
-    }
+  if (documentario) {
+    res.status(200).json({
+      success: true,
+      documentario: documentario,
+    });
+  }
 
-    res.status(400).json({
-        success: false,
-        message: "Documentario não encontrado"
-    })
+  res.status(400).json({
+    success: false,
+    message: "Documentario não encontrado",
+  });
 };
 
 const createDocumentarios = (req, res) => {
-    const { id, titulo, tema, diretor, duracao, anoLancamento, plataforma, avaliacao, categoria } = req.body;
+  const {id, titulo, tema, diretor, duracao, anoLancamento, plataforma, avaliacao, categoria} = req.body;
 
-if (!titulo) {
+  if (!titulo) {
     return res.status(400).json({
-        success: false,
-        message: "O campo 'titulo' é obrigatório"
+      success: false,
+      message: "O campo 'titulo' é obrigatório",
     });
-}
+  }
 
-if (!tema) {
+  if (!tema) {
     return res.status(400).json({
-        success: false,
-        message: "O campo 'tema' é obrigatório"
+      success: false,
+      message: "O campo 'tema' é obrigatório",
     });
-}
+  }
 
-if (!diretor) {
+  if (!diretor) {
     return res.status(400).json({
-        success: false,
-        message: "O campo 'diretor' é obrigatório"
+      success: false,
+      message: "O campo 'diretor' é obrigatório",
     });
-}
+  }
 
-//Duração deve ser maior que 30 minutos
-if (!duracao) {
+  //Duração deve ser maior que 30 minutos
+  if (!duracao || duracao < 30) {
     return res.status(400).json({
-        success: false,
-        message: "O campo 'titulo' é obrigatório"
+      success: false,
+      message: "O campo 'titulo' é obrigatório",
     });
-}
+  }
 
-}
+  if (!anoLancamento) {
+    return res.status(400).json({
+      success: false,
+      message: "O campo 'anoLancamento' é obrigatório",
+    });
+  }
 
+  if (!plataforma) {
+    return res.status(400).json({
+      success: false,
+      message: "O campo 'plataforma' é obrigatório",
+    });
+  }
 
-export { getAllDocumentarios, getDocumentariosById };
+  //Avaliação deve estar entre 0 e 10
+  if (!avaliacao || avaliacao <= 0 || avaliacao >= 10) {
+    return res.status(400).json({
+      success: false,
+      message: "O campo 'avaliacao' é obrigatório e deve estar entre 1 e 10!",
+    });
+  }
 
+  if (!categoria) {
+    return res.status(400).json({
+      success: false,
+      message: "O campo 'categoria' é obrigatório",
+    });
+  }
+
+  const novoDocumentario = {
+    id: 11,
+    titulo: "Wild Wild Country",
+    tema: "Cultos e Conflitos Sociais",
+    diretor: "Chapman Way e Maclain Way",
+    duracao: "6 episódios",
+    anoLancamento: 2018,
+    plataforma: "Netflix",
+    avaliacao: 8.0,
+    categoria: "Série Documental",
+  };
+
+  documentarios.push(novoDocumentario);
+  res.status(201).json({
+    success: true,
+    message: "Documentario cadastrado!",
+    documentario: novoDocumentario,
+  });
+};
+
+export { getAllDocumentarios, getDocumentariosById, createDocumentarios  };
--- src/routes/documentariosRoutes.js
+++ src/routes/documentariosRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { getAllDocumentarios, getDocumentariosById } from "../controllers/documentariosController.js";
+import { createDocumentarios, getAllDocumentarios, getDocumentariosById, createDocumentarios } from "../controllers/documentariosController.js";
 
 
 const router = express.Router();
 
 router.get("/", getAllDocumentarios);
 router.get("/:id", getDocumentariosById);
+router.post("/", createDocumentarios);
 
 export default router;
